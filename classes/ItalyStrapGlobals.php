988f4f93ec6e5a3b0c664fe7f95f2b2e34fa58c5
overclokk <overclokk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 18:32:47 +0000

    Update to 1.2.0
    git-svn-id: https://plugins.svn.wordpress.org/italystrap/tags/1.2.0@1101403 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ classes/ItalyStrapGlobals.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * This is a static class for append some inline javascript and print it in footer
+ * @link  http://coderrr.com/php-passing-data-between-classes/
+ */
+class ItalyStrapGlobals{
+
+    public static $global = '';
+ 
+    public static function set( $data ){
+
+        return self::$global .= $data;
+
+    }
+     
+    public static function get(){
+
+        return self::$global;
+
+    }
+}
+
+class ItalyStrapGlobalsCss{
+
+    public static $global = '';
+ 
+    public static function set( $data ){
+
+        return self::$global .= $data;
+
+    }
+     
+    public static function get(){
+
+        return self::$global;
+
+    }
+}
